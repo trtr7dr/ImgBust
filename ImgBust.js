517e5424c24f8a2031e20fe86b161f56d0181f58
Glo <trtr7dr@users.noreply.github.com>
2018-12-27 12:00:41 +0300

    version 0.9

---
+++ ImgBust.js
@@ -0,0 +1,52 @@
+
+jQuery(function () {
+     /**
+     * Создание экземпляра ImgBust.
+     *
+     * @constructor
+     * @param {string} elem - id картинки.
+     * @param {array} urls - список картинок в порядке необходимого следования.
+     * @param {number} speed - частота смены кадров в миллисекундах.
+     */
+    function ImgBust(elem, urls, speed) { // version 0.9
+
+        if (!jQuery("*").is(jQuery(elem))) {
+            console.error('ImgBust error: element ' + elem + ' not found');
+            return false;
+        }
+        if (urls.length < 1) {
+            console.error('ImgBust error: not found image urls');
+            return false;
+        }
+        this.elem = jQuery(elem);
+        this.tag = jQuery(name);
+        this.urls = urls;
+        this.len = urls.length;
+        this.speed = speed;
+        this.timer = '';
+        this.step = 0;
+        this.flag = false;
+
+        this.stop = function () {
+            this.flag = true;
+        };
+        this.bust = function () {
+            if (this.step === this.len - 1) {
+                this.step = 0;
+            }
+            this.elem.attr('src', this.urls[this.step]);
+            this.step++;
+        };
+
+        this.start = function () {
+            var interval = setInterval(function (img_bust) {
+                if (!this.flag) {
+                    img_bust.bust();
+                } else {
+                    clearInterval(interval);
+                }
+            }, this.speed, this);
+        };
+    }
+    //
+});
